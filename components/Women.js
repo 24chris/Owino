ad6810e3791984915fda7fdd55aeb5f265d9ab1d
24chris <40150356+24chris@users.noreply.github.com>
2022-05-27 08:41:16 +0300

    women

--- components/Women.js
+++ components/Women.js
@@ -12,10 +12,10 @@ export default function Women({ womens }) {
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-0 px-4 sm:py-0 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="pt-10 pb-6 flex flex-col items-center">
-            <h2 className="text-4xl mb-3">Women's Clothes</h2>
+            <h2 className="text-4xl mb-3">Women Clothes</h2>
             <p className="text-gray-600 text-sm"></p>
           </div>
-          <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8">
+          {/* <div className="mt-6 grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8">
             {alls.map((all) => (
               <Link href={`product/${encodeURIComponent(all.get_absolute_url)}`}>
               <a>
@@ -48,7 +48,7 @@ export default function Women({ womens }) {
               </a>
               </Link>
              ))}
-          </div>
+          </div> */}
         </div>
       </div>
     </>
