afdd5e02a1439a4c0c26e74691d30b2836ab1f11
24chris <40150356+24chris@users.noreply.github.com>
2022-06-02 23:12:17 +0300

    imgSlider

--- components/HomeSlider.js
+++ components/HomeSlider.js
@@ -13,9 +13,9 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 
 // Image import 
 import Image from 'next/image'
-import PicOne from '../public/items/banner.jpg'
-import PicTwo from '../public/items/banner2.jpg'
-import PicThree from '../public/items/menbag.jpg'
+import PicOne from '../public/items/new_2(3).jpg'
+import PicTwo from '../public/items/new_2(2).jpg'
+import PicThree from '../public/items/new_2(1).jpg'
 import PicFour from '../public/items/slider4.jpg'
 import PicFive from '../public/items/slider5.jpg'
 
