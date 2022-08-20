b02aba92c03fb4af0876f3d3b6831b35b60fbc7e
24chris <40150356+24chris@users.noreply.github.com>
2022-08-20 11:46:52 +0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,32 @@ import '../styles/globals.css'
 import  "@heroicons/react/outline";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
+
+//Analytics
+import { useEffect } from 'react'
 import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return(
     <>
-   <Script strategy="LazyOnload" async src={'https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'}/>
+   <Script strategy="afterInteractive" async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
     <Script id='google-analytics-script' strategy="LazyOnload">
       {`
       window.dataLayer = window.dataLayer || [];
